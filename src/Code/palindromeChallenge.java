41a665caf43ba4fbcde1a356e1df8f97538999b7
Luiz Lohn <luiz.lohn@gmail.com>
2016-03-01 23:24:58 -0300

    Revert "Revert "RegExp""
    This reverts commit 6b56f1c70df90370f94d36499b3ae7261077ab83.

---
+++ src/Code/palindromeChallenge.java
@@ -0,0 +1,26 @@
+package Code;
+import javax.swing.JOptionPane;
+
+
+public class palindromeChallenge {
+	public static void main(String[] args) {
+		//Solicita a palavra ou frase
+		String text = JOptionPane.showInputDialog("Qual a Palavra ou Frase?");
+		//Pega o texto e tudo que for espaço ele irá remover
+		String text_final = text;
+		text = text.replaceAll("\\s", "");
+		text = text.toUpperCase();
+		//Cria String result
+		String result=""; 
+		for(int i=text.length()-1;i>=0;i--){
+			result+= text.charAt(i);
+		}
+
+		if(result.equals(text)){
+			JOptionPane.showMessageDialog(null, "The Text: "+text_final+ " is a palindrome");
+		}else{
+			JOptionPane.showMessageDialog(null, "The Text: "+text_final+ " isn't a palindrome");;
+		}
+	}
+
+}
